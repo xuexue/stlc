764046f820a288ccd6dfe33bdd92febb4a94efed
gregr <greg.weiqi@gmail.com>
2016-12-27 19:32:39 -0500

    Define eval for lambdas

--- lc.rkt
+++ lc.rkt
@@ -1,11 +1,17 @@
 #lang racket
 
-(define (eval-term env name)
+(define (env-lookup env name)
   (match env
     (`((,n ,val) . ,env)
       (if (eq? n name)
-        val                     ;; Reference binding
-        (eval-term env name)))  ;; Traverse environment
+        val                      ;; Reference binding
+        (env-lookup env name)))  ;; Traverse environment
     ('() (error (format "unbound variable: ~a" name)))))
+
+(define (eval-term env term)
+  (match term
+    ((? symbol?) (env-lookup env term))
+    (`(lambda ,name ,body)
+      `(closure ,name ,body ,env))  ;; Close over environment
 
 (define (eval term) (eval-term '() term))
