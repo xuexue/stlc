4c24ec7bc3ea6d3a17a99ddc75ca8199253eb892
gregr <greg.weiqi@gmail.com>
2016-12-27 19:36:24 -0500

    Define eval for applications

--- lc.rkt
+++ lc.rkt
@@ -8,10 +8,18 @@
         (env-lookup env name)))  ;; Traverse environment
     ('() (error (format "unbound variable: ~a" name)))))
 
+(define (env-extend env name val) `((,name ,val) . ,env))
+
 (define (eval-term env term)
   (match term
     ((? symbol?) (env-lookup env term))
     (`(lambda ,name ,body)
       `(closure ,name ,body ,env))  ;; Close over environment
+    (`(,fn ,arg)                    ;; Apply closure
+      (match (eval-term env fn)
+        (`(closure ,name ,body ,cenv)
+          (eval-term
+            (env-extend cenv name (eval-term env arg))
+            body))))))
 
 (define (eval term) (eval-term '() term))
